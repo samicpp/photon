1f8c9aeacd1c0f65eea1a0ad6910fd7ab666df3c
samic++ <sami.cplusplus0@gmail.com>
2026-03-14 15:43:27 +0100

    fix ffi not working

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+#[cfg(feature = "asyncffi")]
+pub use ffihttp::ffi::{
+    utils::*,
+    http2::*,
+    client::*,
+    server::*,
+    websocket::*,
+    tls_server::*,
+};
+#[cfg(feature = "asyncffi")]
+pub use httprs_core::ffi::own::*;
+
+pub use quic::*;
+pub use http::*;
 
 #[test]
 fn five_is_five() {
